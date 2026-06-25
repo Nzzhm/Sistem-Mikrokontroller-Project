5bd0fca7e23e7fdd1687e5cec462fec92e8ebb81
nadhifsheva <shevanadhif@gmail.com>
2026-06-25 10:54:19 +0700

    Update

--- smarthome_outside.ino
+++ smarthome_outside.ino
@@ -1,6 +1,37 @@
 #include <ESP32Servo.h>
 
-//Pin Komponen
-#define RAIN_SENSOR  14
-#define SERVO_PIN    27
-#define LAMPU_PIN    26
+Servo s1;
+
+#define RAIN_SENSOR 27
+#define SERVO_PIN 18
+
+int val = 0;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    pinMode(RAIN_SENSOR, INPUT);
+
+    s1.attach(SERVO_PIN);
+}
+
+void loop()
+{
+    val = digitalRead(RAIN_SENSOR);
+
+    Serial.println(val);
+
+    if(val == LOW)
+    {
+        Serial.println("Hujan");
+        s1.write(90);
+    }
+    else
+    {
+        Serial.println("Tidak Hujan");
+        s1.write(0);
+    }
+
+    delay(100);
+}
